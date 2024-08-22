f09883489bc68232987d870e1cbae5cbc6d3f4c9
vignesh <vigneshfornavy@gmail.com>
2024-08-22 14:09:42 +0530

    Updated Ui Changes to Header COmponent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,31 @@
 import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { CiUser } from "react-icons/ci";
+import { CiBellOn } from "react-icons/ci";
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center bg-white px-10 h-12">
-      <div>
-        <h3>
-          Home <span className="font-bold text-blue-900">DashBoard V2</span>
+      <div className="cursor-pointer">
+        <h3 className="font-semibold text-gray-400">
+          Home &#x2192;
+          <span className="font-bold text-blue-900"> DashBoard V2</span>
         </h3>
       </div>
       <div className="flex items-center">
         <div className="hidden md:block h-full w-[500px]">
           <input
-            className="bg-gray-200 w-full pl-5 h-8 rounded-md"
+            className="bg-slate-200 w-full pl-5 h-8 rounded-md"
             type="text"
-            placeholder="Search anything..."
+            placeholder="🔍 Search anything..."
           />
         </div>
-        <div className="ml-4">
-          <p>User</p>
+        <div className="ml-4 flex cursor-pointer">
+          <CiBellOn
+            className="mr-5"
+            style={{ width: "25px", height: "20px" }}
+          />
+          <CiUser style={{ width: "25px", height: "20px" }} />
         </div>
       </div>
     </header>
